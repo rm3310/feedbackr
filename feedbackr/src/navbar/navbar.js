ca5e637ca751a0b86d35c304b5daeaf773cc05d9
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2020-08-04 17:56:48 +0300

    Feat: Navbar fully styled with logo

--- feedbackr/src/navbar/navbar.js
+++ feedbackr/src/navbar/navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function NavBar (props) {
   return (
     <div className="navbar">
-      <div className="logo"><img src="../sass/images/Feedbackr.png" alt="Feedbackr"/></div>
+      <div className="logo">Feedbackr</div>
       <ul className="navbar__ul">
         <li className="navbar__li"><a href="/create-quiz">Create Quiz</a></li>
         <li className="navbar__li"><a href="/view-quizzes">View quizzes</a></li>
