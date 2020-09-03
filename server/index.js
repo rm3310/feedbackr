637eb237ed511d4fe29ef779d5204e7f176f65fd
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2020-09-03 17:26:42 +0300

    FEAT: Deployment scripts written

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
+const path = require('path');
 express = require ('express');
 const app = express();
+require('dotenv').config()
+const publicPath = path.join(__dirname, '..', 'client', 'build');
 
 const router = require('./router');
 const cors = require('cors');
 
-PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 app.listen(PORT, () => {
-  console.log(`server listening on http://localhost:${PORT}`); // eslint-disable-line no-console
+  console.log(`server listening on ${PORT}`); // eslint-disable-line no-console
 });
--- server/model/index.js
+++ server/model/index.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/quiz_db',
+const URI = `mongodb+srv://adminUser:${process.env.MONGODB_PASSWORD}@feedbackrcluster.u9tjq.mongodb.net/feedbackr?retryWrites=true&w=majority`
+// dbname feedbackr?
+
+// const connectDB = async () => {
+// await  
+mongoose.connect(URI , //'mongodb://localhost:27017/quiz_db',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true 
   });
+  // console.log('db connected');
+// }
 
 module.exports = mongoose;
+// module.exports = connectDB;
