2ed30c78466fb5be0a0c21efd8ba2b5ed99977fd
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2020-11-23 14:57:55 +0300

    FIX: Switch to MongoDB Atlas Database Server from Local Server

--- server/model/index.js
+++ server/model/index.js
@@ -3,28 +3,28 @@ const mongoose = require('mongoose');
 const URI = `mongodb+srv://adminUser:${process.env.MONGODB_PASSWORD}@feedbackrcluster.u9tjq.mongodb.net/feedbackr?retryWrites=true&w=majority`
 
 // FOR LOCAL TESTING
-mongoose.connect('mongodb://localhost:27017/quiz_db',
+// mongoose.connect('mongodb://localhost:27017/quiz_db',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   });
+
+// mongoose.connection.once("open", function() {
+//   console.log("MongoDB database connection established successfully");
+// });
+// // // //
+
+// CONNECTION TO MONGODB ATLAS
+mongoose.connect(URI,
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
+    useUnifiedTopology: true 
+  }
+);
 
 mongoose.connection.once("open", function() {
   console.log("MongoDB database connection established successfully");
 });
-// // // //
-
-// CONNECTION TO MONGODB ATLAS
-// mongoose.connect(URI,
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true 
-//   }
-// );
-// 
-// mongoose.connection.once("open", function() {
-//   console.log("MongoDB database connection established successfully");
-// });
 // // // //
 
 module.exports = mongoose;
